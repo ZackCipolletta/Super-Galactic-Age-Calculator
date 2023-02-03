c0e6cafa1814beb92235a436dcbbfa0b6280bf10
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-02-03 14:51:08 -0800

    start generating UI to display a persons age on other planets.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './js/triangle.js';
-import Rectangle from './js/rectangle.js';
+import GalacticAge from './js/galacticAge.js';
 
-function handleTriangleForm() {
-  event.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const triangle = new Triangle(length1, length2, length3);
-  const response = triangle.checkType();
-  const pTag = document.createElement("p");
-  pTag.append(`Your result is: ${response}.`);
-  document.querySelector('#response').append(pTag);
+function createGalacticAge() {
+  const userAge = parseInt(document.querySelector('#userAge').value);
+  const userGalacticAge = new GalacticAge(userAge);
+  return userGalacticAge;
 }
 
-function handleRectangleForm() {
-  event.preventDefault();
-  document.querySelector('#response2').innerText = null;
-  const length1 = parseInt(document.querySelector('#rect-length1').value);
-  const length2 = parseInt(document.querySelector('#rect-length2').value);
-  const rectangle = new Rectangle(length1, length2);
-  const response = rectangle.getArea();
-  const pTag = document.createElement("p");
-  pTag.append(`The area of the rectangle is ${response}.`);
-  document.querySelector('#response2').append(pTag);
+function clearResponseTest(element) {
+  document.querySelector(element).innerText = null;
 }
+
+function hanldeMercuryAgeForm() {
+    event.preventDefault();
+    clearResponseTest('#mercuryAge');
+    const userAgeOnMercury = createGalacticAge().ageOnMercury();
+    const pTag = document.createElement("p");
+    pTag.append(`Your age is: on Mercury is: ${userAgeOnMercury}.`);
+    document.querySelector('#mercuryAge').append(pTag);
+}
+
 
 window.addEventListener("load", function() {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
-  document.querySelector("#rectangle-area-form").addEventListener("submit", handleRectangleForm);
+  document.querySelector("#mercury-age-form").addEventListener("submit", hanldeMercuryAgeForm);
+
+  document.querySelector("#venus-age-form").addEventListener("submit", createGalacticAge);
 });
--- src/js/galacticAge.js
+++ src/js/galacticAge.js
@@ -74,19 +74,4 @@ export default class GalacticAge {
     let yearsYetToPassOnJupiter = new GalacticAge(-yearsYetToPassUntilLaterAge);
     return yearsYetToPassOnJupiter.ageOnJupiter();
   }
-
-
-
-
-  //   checkType() {
-  //     if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
-  //       return "not a triangle";
-  //     } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
-  //       return "scalene triangle";
-  //     } else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
-  //       return "equilateral triangle";
-  //     } else {
-  //       return "isosceles triangle";
-  //     }
-  //   }
 }
