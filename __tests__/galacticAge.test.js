4d7c06aee59e8b82eba722e62bd3ca42498f3da5
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-02-03 10:57:22 -0800

    update tests to utilize beforeEach()

--- __tests__/galacticAge.test.js
+++ __tests__/galacticAge.test.js
@@ -2,37 +2,42 @@ import GalacticAge from "../src/js/galacticAge.js";
 
 describe('GalacticAge', () => {
 
-    test("should correctly create a new GalacticAge objec wiht age equal to the input age parameter.", () => {
-        const inputAge = new GalacticAge(34);
-        expect(inputAge.age).toEqual(34);
-    });
+  let inputAge;
 
-    // expect(age.venus).toEqual(4);
-    // expect(age.mars).toEqual(5);
+  beforeEach(() => {
+    inputAge = new GalacticAge(35);
+  });
 
-    test('should correctly determine age on Mercury using a given age value', () => {
-        const ageOnMercury = new GalacticAge(35);
-        expect(ageOnMercury.ageMercury()).toEqual(145.83);
-    });
+  test("should correctly create a new GalacticAge objec wiht age equal to the input age parameter.", () => {
+    expect(inputAge.age).toEqual(35);
+  });
 
+  test('should correctly determine age on Mercury using a given age value', () => {
+    expect(inputAge.ageMercury()).toEqual(145.83);
+  });
+
+  test('should correctly determine age on Mercury using a given age value', () => {
+    const ageOnMercury = new GalacticAge(35);
+    expect(ageOnMercury.ageMercury()).toEqual(145.83);
+  });
 
   //   test('should correctly determine whether three lengths are not a triangle', () => {
   //     const notTriangle = new Triangle(3, 9, 22);
   //     expect(notTriangle.checkType()).toEqual("not a triangle");
   // });
 
-    // test('should correctly determine whether three lengths make a scalene triangle', () => {
-    //     const scalTriangle = new Triangle(4, 5, 7)
-    //     expect(scalTriangle.checkType()).toEqual("scalene triangle");
-    // });
+  // test('should correctly determine whether three lengths make a scalene triangle', () => {
+  //     const scalTriangle = new Triangle(4, 5, 7)
+  //     expect(scalTriangle.checkType()).toEqual("scalene triangle");
+  // });
 
-    // test('should correctly determine whether three lengths make an isosceles triangle', () => {
-    //     const isoscTriangle = new Triangle(5,5,7)
-    //     expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
-    //   });
-    //   test('should correctly determine whether three lengths make an equilateral triangle', () => {
-    //     const equiTriangle = new Triangle(5,5,5)
-    //     expect(equiTriangle.checkType()).toEqual("equilateral triangle");
-    //   });
-    });
+  // test('should correctly determine whether three lengths make an isosceles triangle', () => {
+  //     const isoscTriangle = new Triangle(5,5,7)
+  //     expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
+  //   });
+  //   test('should correctly determine whether three lengths make an equilateral triangle', () => {
+  //     const equiTriangle = new Triangle(5,5,5)
+  //     expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  //   });
+});
 
